92a60eba544075064dddd69ecf7722ccfd192eeb
Ken Bonny <bonny.ken@gmail.com>
2015-11-08 19:31:27 +0100

    Added word cloud of skills (and a more clear table) to the view.

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,38 @@ $(document).ready(function () {
         }
     })(jQuery);
 
-    $.getJSON("../json_data/skills.json", function(skillData) {
-        WordCloud(document.getElementById("skillCanvas"), skillData);
+    /* set the tabs */
+    $("#skillTabs a").click(function(e) {
+        //e.preventDefault();
+        $(this).tab("show");
     });
+
+    $.getJSON("../json_data/skills.json", function(skillData) {
+        /* set the word cloud */
+        var skillCanvas = document.getElementById("skillCanvas");
+        var ctx = skillCanvas.getContext("2d");
+        ctx.canvas.width = window.innerWidth * 0.5;
+        ctx.canvas.height = window.innerHeight * 0.5;
+        WordCloud(skillCanvas, skillData);
+
+        /* set skill list */
+        skillData.list.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        $("#skillTable").append("<tr>");
+        for (var i = 0; i < skillData.list.length; i++) {
+            var skill = skillData.list[i][0];
+            if (i % 6 === 0 & i !== 0) {
+                $("#skillTable").append("</tr>");
+                $("#skillTable").append("<tr>");
+                
+            }
+            $("#skillTable").append("<td>" + skill + "</td>");
+        }
+        $("#skillTable").append("</td>");
+    });
+
+    //$("#skillTabs a:last").tab("show");
+    //$("#skillTabs a:first").tab("show");
 });
